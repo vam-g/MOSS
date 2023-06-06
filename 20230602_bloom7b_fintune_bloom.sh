8bc2e888c68adc25a35db59ce857e202582b900d
Van73 <weizaiff@163.com>
2023-06-06 11:02:30 +0800

    feat(bloom): 添加推理的代码：infer_bloom.py& 20230606infer_bloom_sft_weight.sh

--- 20230602_bloom7b_fintune_bloom.sh
+++ 20230602_bloom7b_fintune_bloom.sh
@@ -2,21 +2,20 @@ num_machines=1
 num_processes=$((num_machines * 8))
 machine_rank=0
 
-out_put_dir="20230531bloom-3b"
+out_put_dir="20230602bloom-7b1"
 accelerate launch \
 	--config_file ./configs/sft.yaml \
 	--num_processes $num_processes \
 	--num_machines $num_machines \
 	--machine_rank $machine_rank \
 	--deepspeed_multinode_launcher standard finetune_bloom.py \
-	--model_name_or_path /data/application/leyf/llm_zoo/bloom3b_yj/bloom-3B \
+	--model_name_or_path /mnt/application/leyf/llm_zoo/bloom7b1 \
 	--data_dir ../data/split_data/ \
 	--output_dir ../output/$out_put_dir \
 	--log_dir ../output/train_logs/$out_put_dir \
 	--n_epochs 10 \
-	--train_bsz_per_gpu 2 \
-	--eval_bsz_per_gpu 2 \
+	--train_bsz_per_gpu 1 \
+	--eval_bsz_per_gpu 1 \
 	--learning_rate 0.000015 \
 	--max_seq_len 1024 \
     --eval_times_per_epoch 2
-	
